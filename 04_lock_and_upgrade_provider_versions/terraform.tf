61fb3b22f5ebf22f369cfe2ab0278e5325b7cb47
Gergo Schreiner <gergo.schreiner@logmein.com>
2023-11-10 10:13:39 +0100

    Observ the lockfile: New provider is added, provider version was modified, provider hashes were pre-populated for 4 platforms.

--- 04_lock_and_upgrade_provider_versions/terraform.tf
+++ 04_lock_and_upgrade_provider_versions/terraform.tf
@@ -9,7 +9,23 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 2.0.0"
     }
+
+    azurerm = {
+      source  = "registry.terraform.io/hashicorp/azurerm"
+      version = "~> 2.12"
+
+    }
+
   }
 
   required_version = ">= 1.1"
 }
+
+// terraform -version
+/*
+terraform providers lock \
+  -platform=linux_arm64 \
+  -platform=linux_amd64 \
+  -platform=darwin_amd64 \
+  -platform=windows_amd64
+*/
