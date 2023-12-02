08566eff18b248253d3195f79c90351fb6713a11
Gergo Schreiner <gergo.schreiner@logmein.com>
2023-12-02 15:46:49 +0100

    [Renaming folder 05]

---
+++ 05_init_validate/learn-terraform-init/unwrapped/tfconfig/m-hello/random.tf
@@ -0,0 +1,40 @@
+variable "hellos" {
+  type = object({
+    hello        = string
+    second_hello = string
+  })
+  description = "list of hellos"
+}
+
+variable "some_key" {
+  type        = string
+  description = "this is a some key"
+}
+
+resource "random_pet" "server" {
+  keepers = {
+    hello      = var.hellos.hello
+    secret_key = var.some_key
+  }
+}
+
+resource "random_pet" "number_2" {
+  keepers = {
+    hello = var.hellos.second_hello
+  }
+}
+
+output "pet" {
+  value       = random_pet.server.id
+  description = "first random pet ID"
+}
+
+output "list_of_pets" {
+  value       = [random_pet.server.id, random_pet.number_2.id]
+  description = "list of random pet IDs"
+}
+
+output "quoted_some_key" {
+  value       = "\"${var.some_key}\""
+  description = "escaped quoted value of some_key"
+}
